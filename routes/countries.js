b39574db839eccc8bb3d169fafe95ccbd1c2553c
Kate Santo <katerine.santo@gmail.com>
2020-01-29 17:21:14 +0000

    API mvp working. Clean up code next

--- routes/countries.js
+++ routes/countries.js
@@ -1,17 +1,17 @@
-const countries = require('./countries.json');
-
+const countries = require('../data/countries.json');
+console.log(countries);
 
 const countryRoutes = (app) => {
     
     app.get('/countries/:countryCode', (req, res) => {
         if (req.params.countryCode) {
             code = req.params.countryCode;
-            // const countries = require('./data/countries.json')
-            // const countryCode = req.params.countryCode;
             const countryName = countries[code];
-            
             res.send(countryName);
-        } else {
+            
+        } else if (!req.params.countryCode) {
+            const countries = require('../data/countries.json');
+            // const allCountries = countries;
             
             res.send(countries);
         }
